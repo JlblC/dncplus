5b3fa4fa2d44ad7d2e5cbbfad2e4aa708ef1af52
jlblc <jlblc@ya.ru>
2012-11-19 18:04:13 +0400

    +

---
+++ dncpmain.js
@@ -0,0 +1,11 @@
+var dncpversion = 'v 0.2.75';
+
+$(document).ready(init);
+
+console.log('DnC+ : Main start ' + dncpversion);
+
+function init() {
+	
+	console.log('DnC+ : Document ready');
+        
+}
